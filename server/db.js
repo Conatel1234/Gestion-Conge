fdf6f7fee280b19bfb266da58adc69a913bffde0
Augustin <augusinroodstanley11@gmail.com>
2026-09-01 15:46:48 -0400

    try fix server

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV === 'production' || process.env.VERCEL 
+    ? { rejectUnauthorized: false } 
+    : false,
 });
 
 module.exports = {
