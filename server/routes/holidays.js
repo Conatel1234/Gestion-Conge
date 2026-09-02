9c792e8c2ff416f1818e94fa4a33409be114dbcb
Augustin <augusinroodstanley11@gmail.com>
2026-09-02 14:04:35 -0400

    try update pdf

--- server/routes/holidays.js
+++ server/routes/holidays.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('../db');
-const { PDFParse } = require('pdf-parse');
+const { PDFParse } = require('pdf-parse/lib/pdf-parse.js');
 
 const router = express.Router();
 
